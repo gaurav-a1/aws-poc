a73eb361906e34edd5c5f06e330817af26493868
Gaurav Singh <gauravks.singh@gmail.com>
2024-01-17 16:10:45 +0530

    Initial Commit

--- src/main/java/com/serverless/Constants.java
+++ src/main/java/com/serverless/Constants.java
@@ -1,6 +1,6 @@
 package com.serverless;
 
 public class Constants {
-    public static final String DYNAMO_DB_TABLE="UserFile3";
-    public static final String BUCKET_NAME="lambdasam-2-42c0f00a90-ap-south-1";
+    public static final String DYNAMO_DB_TABLE="";
+    public static final String BUCKET_NAME="";
 }
