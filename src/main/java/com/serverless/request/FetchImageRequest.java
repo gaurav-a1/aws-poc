980ffbbb90e659d8a5170518b696245cdf414d64
Gaurav Singh <gauravks.singh@gmail.com>
2024-09-10 19:30:45 +0530

    dependency version upgrade, pom restructure.

--- src/main/java/com/serverless/request/FetchImageRequest.java
+++ src/main/java/com/serverless/request/FetchImageRequest.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class FetchImageRequest {
     private String startDate;
     private String endDate;
